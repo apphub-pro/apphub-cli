768070d1c0760a2624abef2f987593dae72136d7
Dmitry Kotlyarov <kotlyarov.dmitry@gmail.com>
2017-01-18 03:23:36 +0300

    added instance join command

--- src/main/java/apphub/cli/command/instance/join/CustomCommand.java
+++ src/main/java/apphub/cli/command/instance/join/CustomCommand.java
@@ -1,8 +1,37 @@
+/*
+ * Copyright (C) 2014 Dmitry Kotlyarov.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package apphub.cli.command.instance.join;
+
+import apphub.util.Command;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Dmitry Kotlyarov
  * @since 1.0
  */
-public class CustomCommand {
+public class CustomCommand extends Command {
+    public CustomCommand(List<String> arguments, Map<String, String> parameters) {
+        super(arguments, parameters);
+    }
+
+    @Override
+    public void run() {
+    }
 }
